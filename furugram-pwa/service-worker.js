0b6cb5e1ad6bedaece7f41d7c7b6241c90912f8f
furucfg2-bit <furucfg2@gmail.com>
2026-05-28 21:40:52 +0300

    Add files via upload

--- furugram-pwa/service-worker.js
+++ furugram-pwa/service-worker.js
@@ -1,29 +1,29 @@
-var CACHE_NAME = "furugram-v1";
-var ASSETS = ["index.html", "admin.html", "manifest.json", "icon-192.png", "icon-512.png"];
+const CACHE_NAME = 'furugram-v1';
+const ASSETS = [
+    '/furugram/',
+    '/furugram/index.html',
+    '/furugram/admin.html',
+    '/furugram/manifest.json',
+    '/furugram/icon-192.png',
+    '/furugram/icon-512.png'
+];
 
-self.addEventListener("install", function(event) {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(function(cache) {
-      return cache.addAll(ASSETS);
-    })
-  );
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    );
 });
 
-self.addEventListener("activate", function(event) {
-  event.waitUntil(
-    caches.keys().then(function(names) {
-      return Promise.all(
-        names.filter(function(name) { return name !== CACHE_NAME; })
-          .map(function(name) { return caches.delete(name); })
-      );
-    })
-  );
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((names) => Promise.all(
+            names.filter((name) => name !== CACHE_NAME).map((name) => caches.delete(name))
+        ))
+    );
 });
 
-self.addEventListener("fetch", function(event) {
-  event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => response || fetch(event.request))
+    );
 });
